14e0d175c3875f2f8f6fc5563fd1138d670a9c0e
developer.kappatech.net <alvin.schurman@gmail.com>
2024-09-02 13:59:10 -0600

    feat: add did login

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+    new webpack.DefinePlugin({
+      'process.env.INFURA_PROJECT_ID': JSON.stringify(process.env.INFURA_PROJECT_ID || 'default_project_id')
+    }),
+  ],
+};
